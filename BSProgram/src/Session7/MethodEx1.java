0f43faacca72c35ce19713bd6c2624df50ea48a5
QUAN <lenhatminhquan@gmail.com>
2023-09-05 14:55:56 +0300

    Update MethodEx1.java
    done Ex2

--- BSProgram/src/Session7/MethodEx1.java
+++ BSProgram/src/Session7/MethodEx1.java
@@ -3,11 +3,15 @@ package Session7;
 public class MethodEx1 {
 	public static void main(String[] args) {
 		printTitle();
+		multiplie1k(5);
 	}
 	static void printTitle() {
 	    System.out.println("********************");
 	    System.out.println("* Method Exercises *");
 	    System.out.println("********************");
 	    
-	  }
+	 }
+	static void multiplie1k(int i) {
+		System.out.println(i*1000);
+	}
 }
