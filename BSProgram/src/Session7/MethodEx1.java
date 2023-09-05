162bf09a3d86e126654690c884944f5f9b66c96e
QUAN <lenhatminhquan@gmail.com>
2023-09-05 15:04:02 +0300

    Update MethodEx1.java
    done ex3

--- BSProgram/src/Session7/MethodEx1.java
+++ BSProgram/src/Session7/MethodEx1.java
@@ -1,9 +1,10 @@
 package Session7;
-
+import java.util.Scanner;
 public class MethodEx1 {
 	public static void main(String[] args) {
 		printTitle();
-		multiplie1k(5);
+		multiplie1k(inputNum());
+		
 	}
 	static void printTitle() {
 	    System.out.println("********************");
@@ -13,5 +14,14 @@ public class MethodEx1 {
 	 }
 	static void multiplie1k(int i) {
 		System.out.println(i*1000);
+	}
+	static int inputNum() {
+		System.out.println("Please, input the number:");
+		Scanner myObj = new Scanner(System.in);
+		String answer="";
+		answer = myObj.nextLine();
+		int i= Integer.parseInt(answer);
+		myObj.close();
+		return i;
 	}
 }
