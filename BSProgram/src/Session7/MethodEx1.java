680deac74661ea5751cad86832132a3952e90196
QUAN <lenhatminhquan@gmail.com>
2023-09-05 14:35:41 +0300

    Create MethodEx1.java

---
+++ BSProgram/src/Session7/MethodEx1.java
@@ -0,0 +1,13 @@
+package Session7;
+
+public class MethodEx1 {
+	public static void main(String[] args) {
+		printTitle();
+	}
+	static void printTitle() {
+	    System.out.println("********************");
+	    System.out.println("* Method Exercises *");
+	    System.out.println("********************");
+	    
+	  }
+}
