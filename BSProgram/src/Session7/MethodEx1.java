c69d3c772881d27422cd461f7c5c5269d9f35b6f
QUAN <lenhatminhquan@gmail.com>
2023-09-18 10:01:13 +0300

    update method ex

--- BSProgram/src/Session7/MethodEx1.java
+++ BSProgram/src/Session7/MethodEx1.java
@@ -1,21 +1,53 @@
 package Session7;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 public class MethodEx1 {
 	public static void main(String[] args) {
 		printTitle();
-		multiplie1k(inputNum());
+		//multiplie1k(inputNum());
+		/*System.out.println("Please, input the width:");
+		Scanner myObj = new Scanner(System.in);
+		String answer="";
+		answer = myObj.nextLine();
+		int width= Integer.parseInt(answer);
+		System.out.println("Please, input the length:");
+		answer = myObj.nextLine();
+		int length= Integer.parseInt(answer);
+		myObj.close();
+		//area(width,length);
+		System.out.println("Area is:" + area2(width,length));
+		if (area2(width,length) > 50) {
+			System.out.println("Huge room");
+		} else {
+			System.out.println("Standard room size");
+		}
+		String[] furnitures = {"table" ,"chair", "shelf","door","sofaa"};
+		for (int i=0; i < furnitures.length;i++) {
+			System.out.println(furnitures[i]);
+		}
+		System.out.println("Soft table is ");
+		printTableSorted(furnitures);*/
+		ArrayList<String> cars = new ArrayList<String>();
+	    cars.add("Volvo");
+	    cars.add("BMW");
+	    cars.add("Ford");
+	    cars.add("Mazda");
+	    System.out.println(cars);
+	    printListSorted(cars);
+		
 		
 	}
-	static void printTitle() {
+	public static void printTitle() {
 	    System.out.println("********************");
 	    System.out.println("* Method Exercises *");
 	    System.out.println("********************");
 	    
 	 }
-	static void multiplie1k(int i) {
+	public static void multiplie1k(int i) {
 		System.out.println(i*1000);
 	}
-	static int inputNum() {
+	public static int inputNum() {
 		System.out.println("Please, input the number:");
 		Scanner myObj = new Scanner(System.in);
 		String answer="";
@@ -23,5 +55,23 @@ public class MethodEx1 {
 		int i= Integer.parseInt(answer);
 		myObj.close();
 		return i;
+	}
+	public static void area(int width, int length) {
+		System.out.println("Area is:" + (width * length));
+	}
+	public static int area2(int width, int length) {
+		int area = width * length;
+		
+		return area;
+	}
+	public static void printTableSorted (String[] table) {
+		Arrays.sort(table);
+		for (int i=0; i < table.length;i++) {
+			System.out.println(table[i]);
+		}
+	}
+	public static void printListSorted (ArrayList <String> table) {
+		table.sort(null);
+		System.out.println(table);
 	}
 }
